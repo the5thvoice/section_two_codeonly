60dc7a114e126c664165feda7524dc45e4492f39
Tanya <tanya.jo.byrne@gmail.com>
2021-04-17 10:57:09 +0100

    hello world

--- trippleX/Scripts/trippleX_main.cpp
+++ trippleX/Scripts/trippleX_main.cpp
@@ -1,4 +1,8 @@
+#include <iostream> // includes do not need ; to function (formal name: Preprocessor Directive)
+
 int main()
 {
+
+	std::cout << "Hello World"; // std is short for standard - it's a namespace. the :: is a scope operator. it is was c# it would look somting like this std.cout("Hello World");
 	return 0;
 }
