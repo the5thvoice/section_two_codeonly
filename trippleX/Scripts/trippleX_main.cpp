d16c8cf888001daa55477d737ef4459556232c71
Tanya <tanya.jo.byrne@gmail.com>
2021-04-17 11:06:53 +0100

    lesson 2

--- trippleX/Scripts/trippleX_main.cpp
+++ trippleX/Scripts/trippleX_main.cpp
@@ -3,6 +3,8 @@
 int main()
 {
 
-	std::cout << "Hello World"; // std is short for standard - it's a namespace. the :: is a scope operator. it is was c# it would look somting like this std.cout("Hello World");
+	std::cout << "you apporach the fork in the road\n"; // std is short for standard - it's a namespace. the :: is a scope operator. it is was c# it would look somting like this std.cout("Hello World");
+	std::cout << std::endl; // std::endl or \n in a sting can do a line return
+	std::cout << "one way is the right way and one leeds to doom\n";
 	return 0;
 }
