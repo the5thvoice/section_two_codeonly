1a99a76d1cc512813174fa6f7193e9cfcb573c6f
Tanya <tanya.jo.byrne@gmail.com>
2021-04-18 11:24:49 +0100

    meaning full names and Unreal Naming Convention

--- trippleX/Scripts/trippleX_main.cpp
+++ trippleX/Scripts/trippleX_main.cpp
@@ -7,15 +7,15 @@ int main()
 	std::cout << std::endl; // std::endl or \n in a sting can do a line return
 	std::cout << "one way is the right way and one leeds to doom\n";
 
-	const int numberOne = 4;
-	const int numberTwo = 2;
-	const int numberThree = 9;
+	const int PuzzleNumberOne = 4;
+	const int PuzzleNumberTwo = 2;
+	const int PuzzleNumberThree = 9;
 	
-	int sum = numberOne + numberTwo + numberThree;
-	int product = numberOne * numberTwo * numberThree;	
+	int PuzzleSum = PuzzleNumberOne + PuzzleNumberTwo + PuzzleNumberThree;
+	int PuzzleProduct = PuzzleNumberOne * PuzzleNumberTwo * PuzzleNumberThree;	
 
 	std::cout << "you see a signg pointing in each direction, the correct sign display 3 number that\n";
-	std::cout << "add up to " << sum << std::endl; // if i use << again i can pas another arguement to the terminal
-	std::cout << "and multiply to togeher to get " << product << std::endl;	
+	std::cout << "add up to " << PuzzleSum << std::endl; // if i use << again i can pas another arguement to the terminal
+	std::cout << "and multiply to togeher to get " << PuzzleProduct << std::endl;	
 	return 0;
 }
